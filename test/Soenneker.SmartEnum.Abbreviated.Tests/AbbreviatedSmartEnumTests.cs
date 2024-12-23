68266e2222f73d82fa582c8dae8e5082675b8e2b
Jake Soenneker <jake@soeneker.com>
2024-12-23 22:18:55 +0000

    Tests

---
+++ test/Soenneker.SmartEnum.Abbreviated.Tests/AbbreviatedSmartEnumTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.SmartEnum.Abbreviated.Tests;
+
+[Collection("Collection")]
+public class AbbreviatedSmartEnumTests : FixturedUnitTest
+{
+    public AbbreviatedSmartEnumTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
